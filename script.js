2ae572ff5d9cf6168c0eb6770a89772956e5f2f7
rynoschni <ryno@schnitke.com>
2022-01-04 15:52:09 -0600

    set step-1

--- script.js
+++ script.js
@@ -1,50 +1,12 @@
 'use strict';
 //get the div items in the game
-const gameDiv = document.getElementById("game");
-const wordDiv = document.getElementById("word");
-const letterDiv = document.getElementById("letters");
-const loadDiv = document.getElementById("loadingBee");
+
 
 // get the div items in the wordsBank
-const wordsBankDiv = document.querySelector("#wordsBank");
-const foundTitleDiv = document.querySelector("#foundTitle");
-const wordsDiv = document.querySelector("#words");
+
 
 //create your buttons
-const a_button = document.createElement("button");
-a_button.setAttribute("id", "a_button");
-a_button.setAttribute("value", "A");
-a_button.classList.add("button","is-warning", "m-2");
-a_button.innerHTML = "A";
 
-const b_button = document.createElement("button");
-b_button.setAttribute("id", "b_button");
-b_button.setAttribute("value", "B");
-b_button.classList.add("button", "is-warning", "m-2");
-b_button.innerHTML = "B";
-
-const c_button = document.createElement("button");
-c_button.setAttribute("id", "c_button");
-c_button.setAttribute("value", "C");
-c_button.classList.add("button", "is-warning", "m-2");
-c_button.innerHTML = "C";
-
-const d_button = document.createElement("button");
-d_button.setAttribute("id", "d_button");
-d_button.setAttribute("value", "D");
-d_button.classList.add("button", "is-warning", "m-2");
-d_button.innerHTML = "D";
-
-const e_button = document.createElement("button");
-e_button.setAttribute("id", "e_button");
-e_button.setAttribute("value", "E");
-e_button.classList.add("button", "is-warning", "m-2");
-e_button.innerHTML = "E";
-
-const addButton = document.createElement('div');
-addButton.setAttribute("id", "add-button");
-addButton.classList.add("button", "is-black");
-addButton.innerHTML = "ADD";
 
 //write the function to add the buttons to your HTML on page load
 //** NOTE: setTimeout is just to slow the load so you can see it. but you could use it to add a CSS loaders and other neat stuff!! :)
