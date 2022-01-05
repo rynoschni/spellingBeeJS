3b2a5fab3e3f52991990dc4a9afa7a5502a60dc8
Alex Degregori <alexdegregori@gmail.com>
2022-01-04 16:34:25 -0800

    Completed Step 1

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 'use strict';
 //get the div items in the game
 
+const gameBoardDIV = document.getElementById('gameboard')
+const loadingBeeDIV = document.getElementById('loadingBee')
+const gameDIV = document.getElementById('game')
+const wordDIV = document.getElementById('word')
+const lettersDIV = document.getElementById('letters')
 
 // get the div items in the wordsBank
+
+const wordsBankDIV = document.querySelector('#wordsBank')
+const foundTitle = document.querySelector('#foundTitle')
+const wordsDIV = document.querySelector('#words')
 
 
 //create your buttons
 
+const a_button = document.createElement('button')
+a_button.classList.add("button", "is-warning", "m-2")
+a_button.setAttribute("value", "A")
+a_button.innerHTML = "A"
+
+const b_button = document.createElement('button')
+a_button.classList.add("button", "is-warning", "m-2")
+a_button.innerHTML = "A"
+
+const c_button = document.createElement('button')
+a_button.classList.add("button", "is-warning", "m-2")
+a_button.innerHTML = "A"
+
+const d_button = document.createElement('button')
+a_button.classList.add("button", "is-warning", "m-2")
+a_button.innerHTML = "A"
+
+const e_button = document.createElement('button')
+a_button.classList.add("button", "is-warning", "m-2")
+a_button.innerHTML = "A"
+
+const f_button = document.createElement('button')
+a_button.classList.add("button", "is-warning", "m-2")
+a_button.innerHTML = "A"
 
 //write the function to add the buttons to your HTML on page load
 //** NOTE: setTimeout is just to slow the load so you can see it. but you could use it to add a CSS loaders and other neat stuff!! :)
