0ceec92c07ffbfa3130accb5a150223162bc79e6
Pranoti Savadi <pranoti.savadi@gmail.com>
2022-01-04 19:39:08 -0500

    updates to step1

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
 'use strict';
 //get the div items in the game
+const gameBoardDIV = document.getElementById('gameboard');
+const loadingBeeDIV = document.getElementById('loadinBee');
+const gameDIV = document.getElementById('game');
+const wordDIV = document.getElementById('word');
+const lettersDIV = document.getElementById('letters');
 
 
 // get the div items in the wordsBank
-
+const wordsBankDIV = document.querySelector('#wordsBank');
+const foundTitleDIV = document.querySelector('#foundTitle');
+const wordsDIV = document.querySelector('#words');
 
 //create your buttons
+const a_button = document.createElement('button');
+a_button.classList.add("button", "is-warning", "m-2");
+a_button.setAttribute("value", "A");
+a_button.innerHTML ="A";
+
+const a_button = document.createElement('button');
+b_button.classList.add("button", "is-warning", "m-2");
+b_button.setAttribute("value", "B");
+b_button.innerHTML ="B";
+
+const a_button = document.createElement('button');
+c_button.classList.add("button", "is-warning", "m-2");
+c_button.setAttribute("value", "C");
+c_button.innerHTML ="C";
+
+const a_button = document.createElement('button');
+d_button.classList.add("button", "is-warning", "m-2");
+d_button.setAttribute("value", "D");
+d_button.innerHTML ="D";
+
+const a_button = document.createElement('button');
+e_button.classList.add("button", "is-warning", "m-2");
+e_button.setAttribute("value", "E");
+e_button.innerHTML ="E";
+
+const a_button = document.createElement('button');
+f_button.classList.add("button", "is-warning", "m-2");
+f_button.setAttribute("value", "F");
+f_button.innerHTML ="F";
+
+const addButton = document.createElement("div");
+addButton.classList.add("button","is-black");
 
 
 //write the function to add the buttons to your HTML on page load
