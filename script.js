049cb0b25a01745274120f5593e430a7fe625441
shaymaalkhafaf <shaymaalkhafaf@gmail.com>
2022-01-04 20:30:11 -0600

    updated

--- script.js
+++ script.js
@@ -23,22 +23,22 @@ b_button.setAttribute("value", "B");
 b_button.innerHTML ="B";
 
 const c_button = document.createElement('button');
-c_button.classList.add("button", "is -warning", "m-2");
+c_button.classList.add("button", "is-warning", "m-2");
 c_button.setAttribute("value", "c");
 c_button.innerHTML ="c";
 
 const d_button = document.createElement('button');
-d_button.classList.add("button", "is -warning", "m-2");
+d_button.classList.add("button", "is-warning", "m-2");
 d_button.setAttribute("value", "d");
 d_button.innerHTML ="d";
 
 const e_button = document.createElement('button');
-e_button.classList.add("button", "is -warning", "m-2");
+e_button.classList.add("button", "is-warning", "m-2");
 e_button.setAttribute("value", "e");
 e_button.innerHTML ="e";
 
 const F_button = document.createElement('button');
-F_button.classList.add("button", "is -warning", "m-2");
+F_button.classList.add("button", "is-warning", "m-2");
 F_button.setAttribute("value", "F");
 F_button.innerHTML ="F";
 
@@ -50,11 +50,28 @@ addButton.classList.add("button", "is-black");
 //** NOTE: setTimeout is just to slow the load so you can see it. but you could use it to add a CSS loaders and other neat stuff!! :)
 
 //using EventListener "DOMContentLoaded", load the add buttons
-
+document.addEventListener("DOMcontentLoaded", () => {
+    console.log("Hello");
+    setTimeout(() => {
+        gameDIV.appendChild(addButton);
+    },3500);
+});
 
 //using an IIFE, load the letter button
 //Why did I put the letters in the IFFE??  HINT: LOOK AT THE CONSOLE LOG AND SEE WHEN IT FIRES.
-
+(() => {
+    setTimeout(() => {
+        loadingBeeDIV.classList.toggle("hide");
+        wordDIV.classList.toggle("hide");
+        wordsBankDIV.classList.toggle("hide");
+        lettersDIV.appendChild(a_button);
+        lettersDIV.appendChild(b_button);
+        lettersDIV.appendChild(c_button);
+        lettersDIV.appendChild(d_button);
+        lettersDIV.appendChild(e_button);
+        lettersDIV.appendChild(F_button);
+    },3000)
+});
 
 //Step-3 write the listeners to take a letter and add to your word div
 
